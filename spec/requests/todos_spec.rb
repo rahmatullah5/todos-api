d95872a5b2ead33a848e5b28a40738fad450a1a0
Muhammad Rahmatullah <rahmatthea5@gmail.com>
2021-06-20 23:26:47 +0700

    Create default todo spec and its unit testing

---
+++ spec/requests/todos_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+# spec/requests/todos_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Todos API', type: :request do
+  # initialize test data
+  let!(:todos) { create_list(:todo, 10) }
+  let(:todo_id) { todos.first.id }
+
+  # Test suite for GET /todos
+  describe 'GET /todos' do
+    # make HTTP get request before each example
+    before { get '/todos' }
+
+    it 'returns todos' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body).size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /todos/:id
+  describe 'GET /todos/:id' do
+    before { get "/todos/#{todo_id}" }
+
+    context 'when the record exists' do
+      it 'returns the todo' do
+        expect(JSON.parse(response.body)).not_to be_empty
+        expect(JSON.parse(response.body)['id']).to eq(todo_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:todo_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
+  # Test suite for POST /todos
+  describe 'POST /todos' do
+    # valid payload
+    let(:valid_attributes) { { title: 'Learn Elm'} }
+
+    context 'when the request is valid' do
+      before { post '/todos', params: valid_attributes }
+
+      it 'creates a todo' do
+        expect(JSON.parse(response.body)['title']).to eq('Learn Elm')
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/todos', params: { title: '' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+      
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Title can't be blank/)
+      end
+    end
+  end
+
+  # Test suite for PUT /todos/:id
+  describe 'PUT /todos/:id' do
+    let(:valid_attributes) { { title: 'Shopping' } }
+
+    context 'when the record exists' do
+      before { put "/todos/#{todo_id}", params: valid_attributes }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  # Test suite for DELETE /todos/:id
+  describe 'DELETE /todos/:id' do
+    before { delete "/todos/#{todo_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
